cd3dfff0c31f4db1075c71d7a6be2db2a4f803fa
yuxiaotu <yuxiaotu1024@qq.com>
2022-02-26 11:59:38 +0800

    develop

---
+++ src/js/borther-1.js
@@ -0,0 +1,9 @@
+const bother_1 = {
+
+  template: `
+    <div>
+      <p>Borther 1</p>
+      <input type="text" />
+    </div>
+  `
+}
---
+++ src/js/borther-2.js
@@ -0,0 +1,8 @@
+const bother_2 = {
+  template: `
+    <div>
+      <p>Borther 2</p>
+      <input type="texxt" />
+    </div>
+  `
+}
